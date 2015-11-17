3fa2f6ad19ecf0f4c18b8532019a2c180d165625
Stephen Wright <stephenewright@gmail.com>
2015-11-17 15:32:11 -0500

    completed phot page and redrafted most to use bootstrap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-//Problem: User when clicking on image goes to a dead end
-//Solution: Create an overlay with the large image - Lightbox
-
+//thanks to treehouse for the code which I modified slightly for this gallery
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $("<p></p>");
@@ -15,32 +13,40 @@ $overlay.append($caption);
 $("body").append($overlay);
 
 //Capture the click event on a link to an image
+
 $("#imageGallery a").click(function(event){
   event.preventDefault();
   var imageLocation = $(this).attr("href");
+
   //Update overlay with the image linked in the link
   $image.attr("src", imageLocation);
-  
+
   //Show the overlay.
   $overlay.show();
-  
+
   //Get child's alt attribute and set caption
   var captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
+    //prevent scrolling
+  var scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+      ];
+      var html = jQuery('html');
+      html.data('scroll-position', scrollPosition);
+      html.data('previous-overflow', html.css('overflow'));
+      html.css('overflow', 'hidden');
+      window.scrollTo(scrollPosition[0], scrollPosition[1]);
 });
 
 //When overlay is clicked
 $overlay.click(function(){
   //Hide the overlay
   $overlay.hide();
+  //restore scrolling
+  var html = jQuery('html');
+     var scrollPosition = html.data('scroll-position');
+     html.css('overflow', html.data('previous-overflow'));
+     window.scrollTo(scrollPosition[0], scrollPosition[1])
+
 });
-
-
-
-
-
-
-
-
-
-
