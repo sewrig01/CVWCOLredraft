3c9413422f65eef02e75d9bd6c753c9d6192a78b
Stephen Wright <stephenewright@gmail.com>
2015-11-18 14:04:25 -0500

    Moved items from content to images. Cleaned up code and directories.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $("<p></p>");
 
-//An image to overlay
-$overlay.append($image);
+
 
 //A caption to overlay
 $overlay.append($caption);
+
+//An image to overlay
+$overlay.append($image);
 
 //Add overlay
 $("body").append($overlay);
@@ -25,8 +27,10 @@ $("#imageGallery a").click(function(event){
   $overlay.show();
 
   //Get child's alt attribute and set caption
+
   var captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
+
     //prevent scrolling
   var scrollPosition = [
         self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
